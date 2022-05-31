39fe4849cb2fe1ab3f35a3e049457dcb306f1f54
Jefferson <jeferson.oliveira@g.globo>
2022-05-30 22:56:23 -0300

    fix color in category

--- src/components/CategoryLabel.js
+++ src/components/CategoryLabel.js
@@ -10,7 +10,7 @@ const CategoryLabel = ({ children }) => {
 
   return (
     <div
-      className={`px-2 py-1 text-gray-100 font-bold rounded bg-${colorKey[children]}-500`}
+      className={`px-2 py-1 text-gray-100 font-bold rounded bg-${colorKey[children]}-600`}
     >
       <Link href={`/blog/category/${children.toLowerCase()}`}>{children}</Link>
     </div>
